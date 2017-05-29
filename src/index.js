4c9bb88fdd1c78de207be680ec5d8819491d78a5
jfw10973 <jfw10973@gmail.com>
2017-05-29 23:57:42 +0800

    add todo

--- src/index.js
+++ src/index.js
@@ -105,6 +105,7 @@ let _fetch = (url, fetchOption) => {
 // todo
 // head delete
 // jsonp
+// unit test
 // https://zh.wikipedia.org/wiki/%E8%B6%85%E6%96%87%E6%9C%AC%E4%BC%A0%E8%BE%93%E5%8D%8F%E8%AE%AE
 
 export default {
