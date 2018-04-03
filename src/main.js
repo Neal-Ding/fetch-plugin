220fe31baff2532e19c4080104d23351dda8bc82
jfw10973 <jfw10973@gmail.com>
2018-04-03 09:59:13 +0800

    add todo

--- src/main.js
+++ src/main.js
@@ -103,7 +103,7 @@ let _fetch = (url, fetchOption) => {
     .then(checkStatus)
 }
 // todo
-// head delete
+// head delete jsonp
 // unit test
 // global setup bug
 // https://zh.wikipedia.org/wiki/%E8%B6%85%E6%96%87%E6%9C%AC%E4%BC%A0%E8%BE%93%E5%8D%8F%E8%AE%AE
