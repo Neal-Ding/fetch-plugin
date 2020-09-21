6976c3832a1e480905de0afe48698041f7376586
jfw10973 <jfw10973@gmail.com>
2020-09-21 17:42:27 +0800

    feat: update dependencies for audit

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,5 @@
-import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
+import babel from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
@@ -28,7 +28,7 @@ export default [{
     babel({
       "include": "src/**",
     }),
-    uglify(),
+    terser(),
     resolve(),
     commonjs()
   ]
@@ -43,7 +43,7 @@ export default [{
     babel({
       "include": "src/**",
     }),
-    uglify(),
+    terser(),
     commonjs()
   ]
 }]
